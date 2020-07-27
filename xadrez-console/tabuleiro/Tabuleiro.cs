bc189434696dc3932815ef6cb12bb6ce222c21b7
Vinicius Spada Melo <vinicius.smelo54@gmail.com>
2020-07-27 13:12:41 -0300

    Add: method that prints the 'Tabuleiro' started

--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -4,12 +4,17 @@
     {
         public int linhas { get; set; }
         public int colunas { get; set; }
-        private Peca[,] pecas { get; set; }
+        private Peca[,] pecas;
 
         public Tabuleiro(int linhas, int colunas){
             this.linhas = linhas;
             this.colunas = colunas;
             this.pecas = new Peca[linhas, colunas];
+        }
+
+        public Peca peca(int linha, int coluna)
+        {
+            return pecas[linha, coluna];
         }
     }
 }
