99ccfc129c03815e2f254bebddb23b23d58a59b5
Vinicius Spada Melo <vinicius.smelo54@gmail.com>
2020-07-27 13:28:24 -0300

    Add: custom exception for invalid position

--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -6,7 +6,8 @@
         public int colunas { get; set; }
         private Peca[,] pecas;
 
-        public Tabuleiro(int linhas, int colunas){
+        public Tabuleiro(int linhas, int colunas)
+        {
             this.linhas = linhas;
             this.colunas = colunas;
             this.pecas = new Peca[linhas, colunas];
@@ -17,10 +18,33 @@
             return pecas[linha, coluna];
         }
 
+        public Peca peca(Posicao pos)
+        {
+            return pecas[pos.linha, pos.coluna];
+        }
+
         public void colocarPeca(Peca p, Posicao pos)
         {
             pecas[pos.linha, pos.coluna] = p;
             p.posicao = pos;
+        }
+
+        public bool posicaoValida(Posicao pos)
+        {
+            if (pos.linha < 0 || pos.linha >= linhas || pos.coluna < 0 || pos.coluna >= colunas)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public void validarPosicao(Posicao pos)
+        {
+            if (!posicaoValida(pos))
+            {
+                throw new TabuleiroException("Posicação inválida!");
+            }
         }
     }
 }
