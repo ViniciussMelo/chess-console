4486ded298e66a83dec03b24b0be155cc2fdb098
Vinicius Spada Melo <vinicius.smelo54@gmail.com>
2020-07-29 12:37:32 -0300

    Add: Method for to pass the game turn

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -16,6 +16,9 @@ namespace xadrez_console
                 {
                     Console.Clear();
                     Tela.imprimirTabuleiro(partida.tab);
+                    Console.WriteLine();
+                    Console.WriteLine("Turno: " + partida.turno);
+                    Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
 
                     Console.WriteLine();
                     Console.Write("Origem: ");
@@ -29,7 +32,7 @@ namespace xadrez_console
                     Console.Write("\nDestino: ");
                     Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
 
-                    partida.executaMovimento(origem, destino);
+                    partida.realizaJogada(origem, destino);
                 }
             }
             catch (TabuleiroException e)
